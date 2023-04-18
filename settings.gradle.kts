9c220ec49c386557320b5133e18ddb4f1a04db77
ellierob <elliotrobot15@gmail.com>
2023-04-19 05:56:01 +0600

    1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,9 +4,19 @@
  * The settings file is used to specify which projects to include in your build.
  *
  * Detailed information about configuring a multi-project build in Gradle can be found
- * in the user manual at https://docs.gradle.org/8.0.2/userguide/multi_project_builds.html
+ * in the user manual at https://docs.gradle.org/8.1/userguide/multi_project_builds.html
  * This project uses @Incubating APIs which are subject to change.
  */
 
-rootProject.name = "andr"
-include("app")
+pluginManagement {
+    // Include 'plugins build' to define convention plugins.
+    includeBuild("build-logic")
+}
+
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+}
+
+rootProject.name = "andr3"
+include("app", "list", "utilities")
